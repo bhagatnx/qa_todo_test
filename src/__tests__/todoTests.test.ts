047a6ae5a723390446b87f81eb3f998f6f586ad5
bhagatnx <nishan.bhagat@gmail.com>
2020-12-09 13:22:48 -0500

    updated, need help

--- src/__tests__/todoTests.test.ts
+++ src/__tests__/todoTests.test.ts
@@ -19,6 +19,8 @@ todoLabel: By = By.css('label');
 todoComplete: By = By.css('checkbox');
 clearCompletedButton: By = By.className('button.clear-completed');
 todoStar: By = By.css('star');
+starBanner: By = By.className('starred');
+todoCount: By = By.className('todo-count')
 
 driver: WebDriver;
 url: string = "https://devmountain.github.io/qa_todos/";
@@ -44,12 +46,53 @@ describe("the todo app", () => {
     await driver.findElement(em.todoInput).sendKeys("Test To-Do\n");
   });
   it("can remove a todo", async () => {
+    let myTodos = await driver.findElements(em.todos);
+    await myTodos
+      .filter(async (todo) => {
+        (await (await todo.findElement(em.todoLabel)).getText()) ==
+          "Test To-Do";
+      })[0]
+      .findElement(em.todoComplete)
+      .click();
     await (await driver.findElement(em.clearCompletedButton)).click();
+    myTodos = await driver.findElements(em.todos);
+    let myTodo = await myTodos.filter(async (todo) => {
+      (await (await todo.findElement(em.todoLabel)).getText()) == "Test To-Do";
+    });
+    expect(myTodo.length).toEqual(0);
   });
   it("can mark a todo with a star", async () => {
+    await driver.wait(until.elementLocated(em.todoInput));
+    let startingStars = await (await driver.findElements(em.starBanner)).length;
 
+    await driver.findElement(em.todoInput).sendKeys("Test To-Do\n");
+    await (await driver.findElements(em.todos))
+      .filter(async (todo) => {
+        (await (await todo.findElement(em.todoLabel)).getText()) ==
+          "Test To-Do";
+      })[0]
+      .findElement(em.todoStar)
+      .click();
+    let endingStars = await (await driver.findElements(em.starBanner)).length;
+    expect(endingStars).toBeGreaterThan(startingStars);
   });
   it("has the right number of todos listed", async () => {
-  
+    await driver.wait(until.elementLocated(em.todoInput));
+
+    let startingTodoCount = await (await driver.findElements(em.todos)).length;
+
+    await driver.findElement(em.todoInput).sendKeys("Test To-Do 1\n");
+    await driver.findElement(em.todoInput).sendKeys("Test To-Do 2\n");
+    await driver.findElement(em.todoInput).sendKeys("Test To-Do 3\n");
+    await driver.findElement(em.todoInput).sendKeys("Test To-Do 4\n");
+    await driver.findElement(em.todoInput).sendKeys("Test To-Do 5\n");
+
+    let endingTodoCount = await (await driver.findElements(em.todos)).length;
+
+    let message = await (await driver.findElement(em.todoCount)).getText();
+
+    expect(endingTodoCount - startingTodoCount).toBe(5);
+    expect(message).toBe(`${endingTodoCount} items left`);
   });
-});
+  });
+
